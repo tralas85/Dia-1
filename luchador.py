3ff430909a6cb9f573ee3eaf87f1c083abbc4c54
eltra <eltrala.85@gmail.com>
2023-12-28 13:39:35 -0600

    Se agrega archivo Luchador.py

---
+++ luchador.py
@@ -0,0 +1,5 @@
+# Indicar cual seria tu nombre de luchador
+color = input("Cual es tu color favorito?: ")
+ciudad = input("Cual ciudad te gustaria visitar?: ")
+
+print(f"Tu nombre de luchador es: El {color} de {ciudad}, \nFelicitaciones!")
